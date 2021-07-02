1fd1a70774e76321d468e02067e313d4ad6a866a
Sam Goddard <hello@samgoddard.co.uk>
2021-07-02 17:38:01 +0100

    testing PSI

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { AnimatePresence } from 'framer-motion'
 
 export default function App({ Component, pageProps }) {
   return (
-    <AnimatePresence exitBeforeEnter>
+    <AnimatePresence>
       <Component {...pageProps} />
     </AnimatePresence>
   )
