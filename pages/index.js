e0ad927dc329419e5a6aa398dfeac4dd7fbf7feb
Sam Goddard <hello@samgoddard.co.uk>
2021-07-02 19:02:11 +0100

    testing cursor follow

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { NextSeo } from 'next-seo';
 import Layout from '@/components/layout'
-import { LazyMotion, domAnimation, m, MotionConfigContext } from "framer-motion"
+import { LazyMotion, domAnimation, m, useMotionValue, useSpring } from "framer-motion"
 import Div100vh from 'react-div-100vh'
 import Image from 'next/image'
 import logo from '@/public/logo.svg'
@@ -29,7 +29,6 @@ export const revealDownDelay = {
 		transition: { duration: 1.4, ease: [0.985, 0.010, 0.315, 0.830] }
 	}
 }
-
 
 export const logoUnderscore = {
 	initial: { x: '-110%' },
@@ -74,6 +73,26 @@ export default function Home() {
   const toggleSamHover = () => setSamHovered(!samHovered);
   const toggleIsaacHover = () => setIsaacHovered(!isaacHovered);
 
+  const cursorX = useMotionValue(-100);
+  const cursorY = useMotionValue(-100);
+
+  const springConfig = { damping: 200, stiffness: 2000 };
+  const cursorXSpring = useSpring(cursorX, springConfig);
+  const cursorYSpring = useSpring(cursorY, springConfig);
+
+  useEffect(() => {
+    const moveCursor = (e) => {
+      cursorX.set(e.clientX - 20);
+      cursorY.set(e.clientY - 20);
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+
+    return () => {
+      window.removeEventListener("mousemove", moveCursor);
+    };
+  }, []);
+
   return (
     <Layout>
       <NextSeo
@@ -107,13 +126,27 @@ export default function Home() {
         >
           <Div100vh className="flex flex-wrap p-4 md:p-5 text-[18px] leading-[24px] md:text-[20px] md:leading-[27px] xl:text-[24px] xl:leading-[31px] selection:bg-black selection:text-off-grey">
 
+            <div className="fixed inset-0 z-10 items-center justify-center hidden md:flex">
+              <div className="group w-[40vw] h-[20vw] bg-black bg-opacity-[2%] relative">
+                <div className="opacity-0 transition ease-in-out duration-500 group-hover:opacity-100">
+                  <m.div
+                    className="cursor"
+                    style={{
+                      translateX: cursorXSpring,
+                      translateY: cursorYSpring,
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+
             <div className={`fixed inset-0 flex items-start md:items-center justify-center transition ease-in-out duration-500 z-0 mt-[35vh] md:mt-0 md:pt-0  ${samHovered || isaacHovered ? 'opacity-0' : 'opacity-100' }`}>
               <m.div variants={logoReveal} className="relative overflow-hidden">
                 <m.div variants={logoUnderscore} className="absolute bottom-0 left-0 mb-[0px] ml-[0px] xl:mb-[1.25px] xl:ml-[1.25px] w-[16px] h-[4px] md:w-[20px] md:h-[5px] xl:w-[23px] bg-black"></m.div>
 
                 <div className="absolute bottom-0 left-0 mb-[0px] ml-[0px] xl:mb-[1.25px] xl:ml-[1.25px] w-[16px] h-[4px] md:w-[20px] md:h-[5px] xl:w-[23px] bg-black opacity-10"></div>
 
-                <div className="overflow-hidden text-black">
+                <div className="overflow-hidden text-black">                  
                   <m.div variants={reveal} className="absolute inset-0 ml-[18px] md:ml-[22px] xl:ml-[24px] bg-off-grey opacity-90 z-10"></m.div>
                   
                   <div className="pointer-events-none w-[200px] md:w-[250px] xl:w-[300px]">
