7914f4c867d00151f553056870c6e14fe96696c7
Sam Goddard <hello@samgoddard.co.uk>
2021-07-07 14:36:51 +0100

    twitter tweaks

---
+++ helpers/seo.config.js
@@ -0,0 +1,13 @@
+export default {
+  defaultTitle: 'ShiftWalk™ — Design + Build Studio',
+  description: 'The mini-studio of Isaac Powell + Sam Goddard, designing and building highly-crafted brands and websites for partners around the world. We have a particular focus on the hospitality, architectural, and creative arts sectors, but we always keep an open mind.',
+  openGraph: {
+    type: 'website',
+    locale: 'en_GB',
+    url: 'https://shiftwalk.studio',
+    site_name: 'ShiftWalk™ — Design + Build Studio',
+  },
+  twitter: {
+    cardType: 'summary_large_image',
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import { ThemeProvider } from 'next-themes'
 import '@/styles/main.css'
 import { AnimatePresence } from 'framer-motion'
+import { DefaultSeo } from 'next-seo'
+import SEO from '../helpers/seo.config'
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
-      <AnimatePresence>
-        <Component {...pageProps} />
-      </AnimatePresence>
-    </ThemeProvider>
+    <>
+      <DefaultSeo {...SEO} />
+
+      <ThemeProvider attribute="class">
+        <AnimatePresence>
+          <Component {...pageProps} />
+        </AnimatePresence>
+      </ThemeProvider>
+    </>
   )
 }
