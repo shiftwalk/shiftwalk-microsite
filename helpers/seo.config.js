2f8e204a4583d5e03ce943a127a8b7fd5ba13573
Sam Goddard <hello@samgoddard.co.uk>
2022-02-16 11:27:43 +0000

    deindex staging

--- helpers/seo.config.js
+++ helpers/seo.config.js
@@ -1,4 +1,6 @@
 export default {
+  // REMOVE WHEN MERGE
+  dangerouslySetAllPagesToNoIndex: true,
   defaultTitle: 'ShiftWalk™ — Design + Build Studio',
   description: 'The mini-studio of Isaac Powell + Sam Goddard, designing and building highly-crafted brands and websites for partners around the world. We have a particular focus on the hospitality, architectural, and creative arts sectors, but we always keep an open mind.',
   openGraph: {
